db72e33443b2dc4dc5758fa370f58423fabdf5ef
pineapple79 <fun.goul.109@gmail.com>
2019-11-13 07:31:50 +0600

    Task1-1

---
+++ task1.py
@@ -0,0 +1,11 @@
+def is_even(number):
+    return True if number % 2 == 0 else False
+
+
+def test_is_even():
+    assert is_even(2) == True
+    assert is_even(3) == False
+    assert is_even(8) =                            = True
+    assert is_even(100) == True
+    assert is_even(101) == False
+print("YOUR CODE IS CORRECT!")
